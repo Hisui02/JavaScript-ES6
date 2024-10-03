b3fa760e42e7cb742ef278a8fe2230ace7ca0e4f
Alvaro <alvarolgleiva@gmail.com>
2024-10-03 10:00:42 +0200

    Añadida validación de usuario

--- js/login.js
+++ js/login.js
@@ -1,13 +1,22 @@
 let user
 let psswd
 
+const USER="root"
+const PASSWORD="1234"
+
 let isLoggedIn=false
 
-user=prompt("Introduce tu usuario (root):")
-if (user) psswd=prompt("Introduce tu contraseña (1234):")
+user=prompt("Introduce tu usuario ("+USER+"):")
+if (user) psswd=prompt("Introduce tu contraseña ("+PASSWORD+"):")
 
 // Validación
-if (user=="root" && psswd=="1234") isLoggedIn=true
+
+if (user==USER && psswd==PASSWORD) isLoggedIn=true
+
+if (user.trim().length<3) {
+    alert("ERROR!\n El usuario debetener añ menos 3 caracteres")
+    isLoggedIn=false
+}
 
 if (!isLoggedIn){
     if(confirm("Usuario o contraseña incorrectos...\n¿Quieres volver a intentarlo?")) location.reload()

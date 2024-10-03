2afb4ed68baf36f375326ccd96173da70bfa8773
Alvaro <alvarolgleiva@gmail.com>
2024-10-03 10:22:20 +0200

    Añadidas especificaciones y validación solicitadas

--- js/login.js
+++ js/login.js
@@ -1,21 +1,34 @@
 const form=document.getElementById("loginForm")
 form.onsubmit=function(e){
+    const USER="root"
+    const PASSWORD="1234"
+
     e.preventDefault()
     let isLoggedIn=false
     const user=document.getElementById("user")
     const psswd=document.getElementById("psswd")
 
-    if (user.value=="root" && psswd.value=="1234") isLoggedIn=true
+    if (user.value==USER && psswd.value==PASSWORD) isLoggedIn=true
+
+    if (user.value.trim().length<3){
+        const texto=form.getElementsByTagName("h5")[0]
+        texto.innerHTML="Usuario demasiado corto"
+        texto.style.color="red"
+        user.style.borderColor="red"
+        return
+    }
 
     if (!isLoggedIn){
         const texto=form.getElementsByTagName("h5")[0]
-        texto.innerHTML="Usuario o contraseña incorrectos"
-        texto.style.color="red"
+        alert("Usuario o contraseña incorrectos")
         user.style.borderColor="red"
         psswd.style.borderColor="red"
     } else{
-        document.getElementById("login").style.display="none";
-        document.getElementById("mainContent").style.display="flex";
+        document.getElementById("greeter").innerHTML="Bienvenido, "+user.value+"!"
+        setTimeout(() => {
+            document.getElementById("login").style.display="none";
+            document.getElementById("mainContent").style.display="flex";
+        }, 3000);
     }
     form.reset()
 }
